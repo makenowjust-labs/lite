e7e3cf977f5ee1dcce292e35a04acf430f9f9cae
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-05-14 00:47:49 +0900

    Refactor lite-gimei generator script

---
+++ modules/lite-gimei/data/README.md
@@ -0,0 +1,4 @@
+Files in here are downloaded from:
+
+- <https://raw.githubusercontent.com/willnet/gimei/main/lib/data/addresses.yml>
+- <https://raw.githubusercontent.com/willnet/gimei/main/lib/data/names.yml>
--- modules/lite-gimei/scripts/gen.rb
+++ modules/lite-gimei/scripts/gen.rb
@@ -6,6 +6,10 @@ def titlecase(s)
   s.gsub(/(?:^|_)([a-z])/) { $1.upcase }
 end
 
+def show_data(data)
+  data.each_slice(100).map { |slice| "    #{slice.map { |vs| vs.join(' ') }.join("\n").inspect}" }.join(" + \"\\n\",\n")
+end
+
 addresses = YAML.load_file("#{__dir__}/../data/addresses.yml")
 names = YAML.load_file("#{__dir__}/../data/names.yml")
 
@@ -14,24 +18,24 @@ puts
 puts '/** Data contains auto generated data constants. */'
 puts 'object Data {'
 
-%w(prefecture city town).each do |key|
+%w[prefecture city town].each do |key|
   data = addresses['addresses'][key]
   puts
   puts "  /** All possible `#{key}` data. */"
   puts "  final lazy val #{titlecase(key)}: IndexedSeq[Word] = Word.load("
   puts '    Seq('
-  puts "      #{data.each_slice(100).map { |slice| slice.map { |vs| vs.join(' ') }.join("\n").inspect }.join(" + \"\\n\",\n      ")}"
+  puts show_data(data)
   puts '    ).mkString'
   puts '  )'
 end
 
-[['last_name'], ['first_name', 'male'], ['first_name', 'female']].each do |keys|
+[%w[last_name], %w[first_name male], %w[first_name female]].each do |keys|
   data = names.dig(*keys)
   puts
-  puts "  /** All possible #{[keys[1], "`#{keys[0]}`"].compact.join(" ")} data. */"
+  puts "  /** All possible #{[keys[1], "`#{keys[0]}`"].compact.join(' ')} data. */"
   puts "  final lazy val #{keys.reverse.map { |s| titlecase(s) }.join}: IndexedSeq[Word] = Word.load("
   puts '    Seq('
-  puts "      #{data.each_slice(100).map { |slice| slice.map { |vs| vs.join(' ') }.join("\n").inspect }.join(" + \"\\n\",\n      ")}"
+  puts show_data(data)
   puts '    ).mkString'
   puts '  )'
 end
