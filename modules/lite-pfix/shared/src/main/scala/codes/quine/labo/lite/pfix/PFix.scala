d12b8f65719225f4363932b37533d7f38f169121
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-08-29 01:07:49 +0000

    Reformat with scalafmt 3.0.1

--- modules/lite-pfix/shared/src/main/scala/codes/quine/labo/lite/pfix/PFix.scala
+++ modules/lite-pfix/shared/src/main/scala/codes/quine/labo/lite/pfix/PFix.scala
@@ -1,7 +1,7 @@
 package codes.quine.labo.lite.pfix
 
-/** PFix is a partially defined fixpoint combinator.
-  * The above "fixpoint combinator" means, `PFix(h).toPartialFunction = h(PFix(h).toPartialFunction)` holds for any `h`.
+/** PFix is a partially defined fixpoint combinator. The above "fixpoint combinator" means, `PFix(h).toPartialFunction =
+  * h(PFix(h).toPartialFunction)` holds for any `h`.
   *
   * This implementation is efficient for combining multiple instances via [[orElse]] method.
   */
@@ -9,22 +9,22 @@ final class PFix[A, B] private (private val fs: Vector[(A => B) => PartialFuncti
 
   /** Converts this into a total function.
     *
-    * The result function handles a value as using this instance as possible.
-    * If this instance cannot handle the value, then it passes to the fallback function.
+    * The result function handles a value as using this instance as possible. If this instance cannot handle the value,
+    * then it passes to the fallback function.
     */
   def toFunction(fallback: A => B): A => B = new PFixFunction[A, B](fs, fallback)
 
   /** Converts this into a partial function.
     *
-    * This method is almost same as `toFunction(_ => throw new MatchError)`,
-    * but the result implements `PartialFunction` correctly.
+    * This method is almost same as `toFunction(_ => throw new MatchError)`, but the result implements `PartialFunction`
+    * correctly.
     */
   def toPartialFunction: PartialFunction[A, B] = new PFixPartialFunction(fs)
 
   /** Returns a new instance combined with this and that instances.
     *
-    * The result instance handles a value as using `this` instance as possible.
-    * If this instance cannot handle the value, then it tries to use `that` instance instead.
+    * The result instance handles a value as using `this` instance as possible. If this instance cannot handle the
+    * value, then it tries to use `that` instance instead.
     */
   def orElse(that: PFix[A, B]): PFix[A, B] = new PFix(fs ++ that.fs)
 }
@@ -33,8 +33,8 @@ object PFix {
 
   /** Creates a new instance from a higher-order function.
     *
-    * The specified function takes a total function to call a future result function recursively,
-    * and returns a partial function handles a value.
+    * The specified function takes a total function to call a future result function recursively, and returns a partial
+    * function handles a value.
     */
   def apply[A, B](f: (A => B) => PartialFunction[A, B]): PFix[A, B] = new PFix(Vector(f))
 
